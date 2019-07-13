7a7b9b1cca410408f57279a0946a2dd5e29b19c6
trucane <trufashionista2013@gmail.com>
2019-07-13 15:36:21 -0500

    WIP get request working for users db

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // implement your API here
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
 
 server.get('/', (req, res) => {
     res.send('Hello Web20 node edition')
+});
+
+server.get('/users', (req, res) =>{
+    db.find().then(db =>{
+        res.status(200).json(db)
+    })
+    .catch(err =>{
+        res.status(500).json(err)
+    })
 });
 
 const port = 5000;
