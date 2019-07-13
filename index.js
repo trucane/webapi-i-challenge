ae3790851c4b0951dd7167a3fee6aade27b445b9
trucane <trufashionista2013@gmail.com>
2019-07-13 15:28:16 -0500

    WIP server up and running

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+
+server.get('/', (req, res) => {
+    res.send('Hello Web20 node edition')
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Yes I am on port 5000') );
